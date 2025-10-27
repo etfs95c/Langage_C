3fd69e99059541f2ab3800455fdce1c42eb40bc5
etfs95c <37842868+etfs95c@users.noreply.github.com>
2025-10-27 20:56:07 +0100

    2

--- premier_projet.c
+++ premier_projet.c
@@ -3,12 +3,14 @@
 int main()
 {
     printf("hello world \n");
-    char a;
-    printf("Appuyez sur [Entrer] pour continuer \n");
-    rewind(stdin);
-    a = getchar();
-    printf("%c \n", a);
-    a++;
-    printf("%c \n %i \n", a, a);
+    int a ;
+    double b ;
+
+    scanf("%i%lf", &a, &b);
+
+    printf("%d \t" "%f \n", a, b);
+    printf("%f \t" "%d \n", b, a);
+    printf("octets de a=%ld ; octets de b=%ld \n", sizeof(a),sizeof(b));
+    printf("Les adresses mémoires de a et b sont, respectivements, %p et %p \n", &a, &b);
     return 0;
 }

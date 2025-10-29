ea97f5146cf682fdf0a53b54f7268955bac7233c
etfs95c <37842868+etfs95c@users.noreply.github.com>
2025-10-29 13:20:52 +0100

    Chap 2.6 ex 1 à 4

--- fonctions.c
+++ fonctions.c
@@ -13,3 +13,69 @@ void dino()
     printf("              |     /          |     /     ~-.     ~- _\n");
     printf("              |_____|          |_____|         ~ - . _ _~_-_\n");
 }
+// Chapitre 2.6 "Mise en Pratique : Manipulation de Variables"
+// Exercice 1 :
+void ch2_6_ex1()
+{
+    int a = 654, b = 5741, c = -456;
+    float d = 3.14f, e = 2.61f;
+    double f = 65468795.5643879435;
+    char g = 'h', h = '!';
+    printf("Les valeurs des ints sont : \na = %i\nb = %i\nc = %i\n", a, b, c);
+    printf("Les valeurs des float sont : \nd = %f\ne = %f\n", d, e);
+    printf("La valeur du double est : \nf = %lf\n", f);
+    printf("Les char sont : \ng = %c\nh = %c\n", g, h);
+}
+
+//Exercice 2 :
+void ch2_6_ex2()
+{
+    char a = '@';
+    printf("%c\n", a);
+    printf("%d\n", a);
+}
+
+//Exercice 3:
+void ch2_6_ex3()
+{
+    char a[] ="TataFaitDuZele\n";
+    int i = 0;
+    while (a[i] != '\0')
+    {
+        putchar(a[i]);
+        i++;
+    }
+}
+
+//Exercice 4 :
+void ch2_6_ex4()
+{
+    char a[] = "contrainte";
+    int i = 0;
+
+    while (a[i] != '\0')
+    {
+        putchar(a[i]);
+        i++;
+    }
+    i = 0;
+    putchar('\n');
+    //contrainte
+    //fourchette
+    a[0] = a[0] + 3;
+    a[2] = a[2] + 7;
+    a[3] = a[3] - 2;
+    a[4] = a[4] - 15;
+    a[5] = a[5] + 7;
+    a[6] = a[6] - 4;
+    a[7] = a[7] + 6;
+    
+    while (a[i] != '\0')
+    {
+        putchar(a[i]);
+        i++;
+    }
+    putchar('\n');
+}
+
+//Exercice 5 :
--- premier_projet.c
+++ premier_projet.c
@@ -1,16 +1,59 @@
 #include <stdio.h>
 
+/*
+void ch2_6_ex4()
+{
+    char a[] = "contrainte";
+    int i = 0;
+
+    while (a[i] != '\0')
+    {
+        putchar(a[i]);
+        i++;
+    }
+    i = 0;
+    putchar('\n');
+    //contrainte
+    //fourchette
+    a[0] = a[0] + 3;
+    a[2] = a[2] + 7;
+    a[3] = a[3] - 2;
+    a[4] = a[4] - 15;
+    a[5] = a[5] + 7;
+    a[6] = a[6] - 4;
+    a[7] = a[7] + 6;
+    
+    while (a[i] != '\0')
+    {
+        putchar(a[i]);
+        i++;
+    }
+    putchar('\n');
+}
+*/
+
 int main()
 {
+    
+    return 0;
+}
+/*int main()
+{
     printf("hello world \n");
-    int a ;
-    double b ;
-
-    scanf("%i%lf", &a, &b);
+    int a = 5;
+    double b = 5.6456845 ;
 
     printf("%d \t" "%f \n", a, b);
     printf("%f \t" "%d \n", b, a);
+
+    scanf("%i", &a);
+    scanf("%lf", &b);
+    rewind(stdin);
+
+    printf("%d \t" "%f \n", a, b);
+    printf("%f \t" "%d \n", b, a); 
     printf("octets de a=%ld ; octets de b=%ld \n", sizeof(a),sizeof(b));
     printf("Les adresses mémoires de a et b sont, respectivements, %p et %p \n", &a, &b);
     return 0;
 }
+    */

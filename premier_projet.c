0578997c48becf8fb691dcf5870eead619d4ab0c
etfs95c <37842868+etfs95c@users.noreply.github.com>
2025-11-02 20:23:16 +0100

    4

--- premier_projet.c
+++ premier_projet.c
@@ -7,9 +7,26 @@ void ch2_6_ex7()
     
 }
 */
+void tableau_ANSI()
+{
+    int row, col, n;
+
+    for (row = 0; row < 11; row++) {
+        for (col = 0; col < 10; col++) {
+            n = 10 * row + col;
+            if (n > 109) break;
+            printf("\033[%dm %3d\033[m", n, n);
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
-    
+    /*printf("\a");
+    printf("\033[0;31m Test\033[m");
+    printf("\ntest");*/
+    tableau_ANSI();
     return 0;
 }
+
